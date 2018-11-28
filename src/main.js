7a0d59ef4a2468c13b65bd993771451b1fac74aa
zfang <747320213@qq.com>
2018-11-28 15:56:42 +0800

    emm

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import router from './router'
+import axios from 'axios'
+import 'babel-polyfill'
 
+Vue.use(Vuetify)
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
